b6fe315b99238a28d2d66ce9f7860580da25410a
Christopher Danek <cdanek@stan1.awi.de>
2021-03-24 16:34:34 +0100

    asd

--- functions/mydiff.r
+++ functions/mydiff.r
@@ -18,14 +18,13 @@ f2 <- readLines(args[2])
 f1_and_f2 <- intersect(f1, f2)
 f1_not_f2 <- f1[which(is.na(match(f1, f2)))]
 f2_not_f1 <- f2[which(is.na(match(f2, f1)))]
-if (length(f1_not_f2) > 0) {
-    cat("\nthese ", length(f1_not_f2), " files are in f1 (", 
-        args[1], ") but not in f2 (", args[2], "):\n\n", sep="")
-    cat(paste(f1_not_f2, collapse="\n"), "\n", sep="")
-}
-if (length(f2_not_f1) > 0) {
-    cat("\nthese ", length(f2_not_f1), " files are in f2 (", args[2], 
-        ") but not in f1 (", args[1], "):\n\n", sep="")
-    cat(paste(f2_not_f1, collapse="\n"), "\n", sep="")
-}
+cat("\n", length(f1_and_f2), " files are in f1 (", 
+    args[1], ") and in f2 (", args[2], ")\n\n", sep="")
+if (length(f1_and_f2) > 0) cat(paste(f1_and_f2, collapse="\n"), "\n", sep="")
+cat("\n", length(f1_not_f2), " files are in f1 (", 
+    args[1], ") but not in f2 (", args[2], ")\n\n", sep="")
+if (length(f1_not_f2) > 0) cat(paste(f1_not_f2, collapse="\n"), "\n", sep="")
+cat("\n", length(f2_not_f1), " files are in f2 (", args[2], 
+    ") but not in f1 (", args[1], ")\n\n", sep="")
+if (length(f2_not_f1) > 0) cat(paste(f2_not_f1, collapse="\n"), "\n", sep="")
 
